82cc142d446034da4d6db12d02ba72c8451d2d30
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2018-11-16 21:02:41 -0200

    Dual Language God
    God command now works in portuguese and english

--- src/main/java/szcommands/szcommands/commands/God.java
+++ src/main/java/szcommands/szcommands/commands/God.java
@@ -6,19 +6,31 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
+
 public class God implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player){
+            String[] alias = {"invulnerable","invincible","god"};
+            String[] frase = new String[2];
+            if(Arrays.asList(alias).contains(label)){
+                frase[0]="You are now "+label;
+                frase[1]="You are no longer "+label;
+            }else{
+                frase[0]="Você está "+label;
+                frase[1]="Você não está mais "+label;
+            }
+
             Player player = (Player) sender;
             if(player.isInvulnerable()){
-                player.sendMessage(ChatColor.GOLD +"You are no longer invulnerable");
+                player.sendMessage(ChatColor.GOLD +frase[1]+label);
                 player.setInvulnerable(false);
             } else{
-                player.sendMessage(ChatColor.GOLD +"You are now invulnerable");
+                player.sendMessage(ChatColor.GOLD +frase[0]+label);
                 player.setInvulnerable(true);
             }
         }
-        return false;
+        return true;
     }
 }
